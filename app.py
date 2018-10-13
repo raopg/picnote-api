88fb0bf10c05515bb5b39308194e3bb84a284d44
raopg <prateek.rao.08@gmail.com>
2018-10-13 13:02:41 -0700

    Updated endpoints based on DB helpers

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 from flask import Flask, jsonify, request
-import authenticate
-from professoraccount import Professor
-from note import Note
-firebase_app = authenticate.authenticate_firebase()
-
-db = firebase_app.database()
+import json
 
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
@@ -22,7 +17,7 @@ def authenticate_api_key(key):
     valid_keys = []
     with open('creds.json','r') as f:
         valid_keys = json.load(f)
-    if valid_keys['test'] == key:
+    if valid_keys['username'] == key:
         return jsonify({'response':'200','message':'API Key Verified'})
     return jsonify({'response':'401','message':'Authentication failed: Illegal API Key'})
 
@@ -40,15 +35,22 @@ def login(key):
     return jsonify({'response':'403','message':'This function only supports the POST method'})
 
 
-@app.route('/api/<string:key>/<string:hashed>/add_course', methods = ['POST'])
-def add_course(key,hashed):
+@app.route('/api/<string:key>/<string:hashed>/prof_add_course', methods = ['POST'])
+def prof_add_course(key,hashed):
     response = authenticate_api_key(key)
     if(response['response'] != '200'):
         return response
     if request.method == "POST":
         course_name = request.form.getlist("cname")
-        section_list = request.form.getlist("")
+        section_list = request.form.getlist("slists")
         
+        course_ID = db.prof_add_course(hashed,cname)
+        for section_name in section_list:
+            db.prof_add_section(course_ID,section_name)
+        return jsonify({'response':'200','course_ID':course_ID})
+    return jsonify({'response':'503','message':'Internal server error'})
+
+
 
 @app.route('/api/<string:key>/<string:hashed>/post_note', methods=['POST'])
 def post_note(key,hashed):
@@ -61,11 +63,19 @@ def post_note(key,hashed):
         
 
 @app.route('/api/<string:key>/<string:hashed>/get_notes', methods=['GET'])
-def get_notes(key, hashed):
+def get_notes_by_id(key, hashed):
     json_return = authenticate_api_key(key)
     if(json_return['response'] != '200'):
         return json_return
-    return 
+    if db.prof_id_exists(hashed):
+        return jsonify(db.get_prof_notes(hashed))
+    elif db.course_id_exists(hashed):
+        return jsonify(db.get_course_notes(hashed))
+    elif db.section_id_exists(hashed):
+        return jsonify(db.get_section_notes(hashed))
+    else:
+        return jsonify({'response':'404','message':'Hashed ID does not match a professor, student or section'})
+
 
 
 
